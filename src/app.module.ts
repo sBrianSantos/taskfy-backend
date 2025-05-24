364a52ca9e7515270b7d39fd0281f62734a5cf0a
Brian <sbriansantos2002@gmail.com>
2025-05-24 02:44:19 -0300

    feat: refactor database connection to use URL by Neon DB and SSL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,16 @@ import { JwtModule } from '@nestjs/jwt';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        url: configService.get<string>('DB_URL'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        // Uncomment the following lines if you want to use individual DB connection parameters
+        // host: configService.get<string>('DB_HOST'),
+        // port: configService.get<number>('DB_PORT'),
+        // username: configService.get<string>('DB_USERNAME'),
+        // password: configService.get<string>('DB_PASSWORD'),
+        // database: configService.get<string>('DB_DATABASE'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         migrations: [__dirname + '/migration/*{.ts,.js}'],
         migrationsRun: true,
